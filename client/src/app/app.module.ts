59b89817a45a6235b55d6805bf96cac876be95b4
Pavel <davidov.pavel@gmail.com>
2018-03-23 10:48:17 +0300

    basic #1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ApiService } from './api.service';
 import { InterestsComponent } from './interests/interests.component';
 import { InteresComponent } from './interests/interes/interes.component';
+
+import { ApiService } from './api.service';
+
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+
+registerLocaleData(localeRu, 'ru');
 
 const routes: Routes = [
   { path: '', redirectTo: '/interests', pathMatch: 'full' },
@@ -20,7 +26,7 @@ const routes: Routes = [
   declarations: [AppComponent, NotFoundComponent, InterestsComponent, InteresComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [ApiService],
+  providers: [ApiService, { provide: LOCALE_ID, useValue: 'ru' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
